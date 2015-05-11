6cca3a5e090d9b4eb6681c1a69426b2ae4a2871b
Espen Halvorsen <espenh@unipluss.no>
2015-05-11 14:41:28 +0200

    Endret url til test-api (og lagt inn .gitignore)

--- AngularJSSPA/App/services.js
+++ AngularJSSPA/App/services.js
@@ -7,35 +7,24 @@ angular.module('app.services', [])
     .value('version', '0.1')
 
     .service('apiService', ['$http', function ($http) {
-        return {
-            getStuff: function (id) {
-                return $http.get('http://localhost:3000/' + id);
-            },
-            putStuff: function (id, data) {
-                return $http.put('http://localhost:3000/' + id, data);
-            },
-            postStuff: function (id, data) {
-                return $http.post('http://localhost:3000/' + id, data);
-            },
-            deleteStuff: function(id) {
-                return $http.delete('http://localhost:3000/' + id);
-            }
-        }
-    }])
 
-    .service('restApiService', ['$resource', function ($resource) {
+        var url = 'http://jsonserver.azurewebsites.net/people';
+
         return {
+            getAllStuff: function() {
+                return $http.get(url);
+            },
             getStuff: function (id) {
-                return $resource.get('http://localhost:3000/' + id);
+                return $http.get(url + '/' + id);
             },
             putStuff: function (id, data) {
-                return $resource.put('http://localhost:3000/' + id, data);
+                return $http.put(url + '/' + id, data);
             },
             postStuff: function (id, data) {
-                return $resource.post('http://localhost:3000/' + id, data);
+                return $http.post(url + '/' + id, data);
             },
             deleteStuff: function (id) {
-                return $resource.delete('http://localhost:3000/' + id);
+                return $http.delete(url + '/' + id);
             }
         }
     }])
@@ -43,7 +32,8 @@ angular.module('app.services', [])
     .service('resolveTestService', ['$http', function ($http) {
         return {
             getResolveResult: function () {
-                return $http.get('http://localhost:3000');
+                return $http.get('http://jsonserver.azurewebsites.net/people');
+                //return $http.get('http://localhost:3000/people');
                 //return $http.get('http://jsonplaceholder.typicode.com/todos');
             }
         }
