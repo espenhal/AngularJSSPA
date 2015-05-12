73a19f80a35d320c83e9180a75672c36b776f472
Espen Halvorsen <espenh@unipluss.no>
2015-05-12 08:21:20 +0200

    fikse manglende glyphicons på azure

--- AngularJSSPA/App_Start/BundleConfig.cs
+++ AngularJSSPA/App_Start/BundleConfig.cs
@@ -14,7 +14,7 @@ namespace App.AngularJSSPA
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/content/css/app").Include("~/content/app.css"));
+            bundles.Add(new StyleBundle("~/content/app").Include("~/content/app.css"));
 
             //bundles.Add(new ScriptBundle("~/js/jquery").Include("~/scripts/vendor/jquery-{version}.js"));
 
